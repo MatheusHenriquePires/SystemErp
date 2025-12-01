f0c644417279738e0760fc719b60900ddd0cb12f
MatheusHenriquePires <matheustv124@gmail.com>
2025-12-01 03:00:42 -0300

    fix: Estrutura final de deploy e scripts lp

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,12 @@
-// Adicionamos esta importação manual para corrigir o erro
-import { defineNuxtConfig } from 'nuxt/config'
-
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
-  
-  css: ['~/assets/css/main.css'],
-
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
-
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-  
+  typescript: {
+    shim: false,
+    strict: true,
+    typeCheck: true,
+    tsConfig: {
+      compilerOptions: {
+        moduleResolution: "bundler"
+      }
+    }
+  }
 })
