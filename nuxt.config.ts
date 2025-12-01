e2de2c1fd501edadaaf75ac8fc532d9e3d835493
MatheusHenriquePires <matheustv124@gmail.com>
2025-12-01 03:08:08 -0300

    fix: add vue-tsc for docker build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,5 @@
 export default defineNuxtConfig({
   typescript: {
-    shim: false,
-    strict: true,
-    typeCheck: true,
-    tsConfig: {
-      compilerOptions: {
-        moduleResolution: "bundler"
-      }
-    }
+    typeCheck: false,
   }
 })
