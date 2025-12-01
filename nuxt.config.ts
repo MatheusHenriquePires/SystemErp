da439b248630ce6c980c24764af6baa65c1414f6
MatheusHenriquePires <matheustv124@gmail.com>
2025-12-01 03:41:02 -0300

    fix: Corrigindo configuracao PostCSS para deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,24 @@
+import { defineNuxtConfig } from 'nuxt/config';
+
 export default defineNuxtConfig({
-  typescript: {
-    typeCheck: false,
-  }
-})
+  // Módulos
+  modules: ['@nuxtjs/tailwindcss'],
+
+  // Linha crucial para carregar o Tailwind
+  css: ['~/assets/css/main.css'], 
+
+  // Configurações do compilador
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
+  // Garanta que o target seja node-server (para o deploy)
+  nitro: {
+    preset: 'node-server'
+  },
+  
+  devtools: { enabled: true },
+});
