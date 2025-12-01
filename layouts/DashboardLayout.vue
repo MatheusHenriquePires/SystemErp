1cf5da1126f3f13634e4744bb88d51638f6ef4a5
MatheusHenriquePires <matheustv124@gmail.com>
2025-12-01 03:38:59 -0300

    fix: Adicionando dependencias PostCSS ausentes para build Docker

--- layouts/DashboardLayout.vue
+++ layouts/DashboardLayout.vue
@@ -10,7 +10,7 @@
       <nav class="flex-1 p-4 space-y-1">
         <p class="px-3 text-xs font-semibold text-slate-400 uppercase tracking-wider mb-2 mt-2">Gestão</p>
         
-        <NuxtLink to="/" class="px-3 py-2 text-blue-600 bg-blue-50 rounded-md font-medium cursor-pointer flex items-center gap-3">
+        <NuxtLink to="/" class="px-3 py-2 text-blue-600 bg-blue-50 rounded-md font-medium cursor-pointer flex items-center gap-3 transition">
           <span class="text-lg">📊</span> Dashboard
         </NuxtLink>
         
@@ -23,6 +23,7 @@
         <NuxtLink to="/clientes" class="px-3 py-2 text-slate-600 hover:bg-slate-50 rounded-md font-medium cursor-pointer flex items-center gap-3 transition">
           <span class="text-lg">👥</span> Clientes
         </NuxtLink>
+        
         <NuxtLink to="/produtos" class="px-3 py-2 text-slate-600 hover:bg-slate-50 rounded-md font-medium cursor-pointer flex items-center gap-3 transition">
           <span class="text-lg">📦</span> Produtos
         </NuxtLink>
@@ -30,20 +31,21 @@
 
       <div class="p-4 border-t border-slate-100">
         <button @click="logout" class="flex items-center gap-2 text-red-600 hover:bg-red-50 w-full px-3 py-2 rounded-md transition font-medium text-sm">
+          <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path></svg>
           Sair do Sistema
         </button>
       </div>
     </aside>
 
     <main class="flex-1 overflow-y-auto">
-        <slot />
+        <slot /> 
     </main>
   </div>
 </template>
 
-<script setup lang="ts">
-// Adicionamos a função logout aqui, já que ela pertence ao layout.
-const cookie = useCookie('usuario_sessao');
+<script setup>
+// Lógica de Logout (Auto-importado pelo Nuxt)
+const cookie = useCookie('usuario_sessao'); 
 function logout() {
   cookie.value = null; // Apaga o cookie
   window.location.href = '/login'; // Redireciona
