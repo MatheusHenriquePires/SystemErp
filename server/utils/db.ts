ca939f1df1c5199d0957c818ec145ef8dd38f3a7
MatheusHenriquePires <matheustv124@gmail.com>
2025-12-05 12:02:30 -0300

    excluir processos errados

---
+++ server/utils/db.ts
@@ -0,0 +1,15 @@
+// server/database.ts
+import postgres from 'postgres'
+
+const connectionString = process.env.DATABASE_URL
+if (!connectionString) {
+  throw new Error("DATABASE_URL não está configurada no ambiente de produção.")
+}
+// Cria uma instância única de conexão para todo o servidor Nitro
+const sql = (globalThis as any).db || postgres(connectionString)
+if (process.env.NODE_ENV !== 'production') {
+  (globalThis as any).db = sql
+  
+}
+
+export default sql // <--- Essa é a conexão que suas APIs devem importar
